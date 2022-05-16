982827a5a8cc13a3af94d5b5a082bcdeed1f7f8b
seymur_orucov <seymur.orucov@gammaskynet.az>
2022-05-17 00:48:36 +0400

    done

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import Vue from "vue";
+import Vuelidate from "vuelidate";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
@@ -7,7 +8,7 @@ import dateFilter from "@/filters/date.filter";
 import "materialize-css/dist/js/materialize.min";
 
 Vue.config.productionTip = false;
-
+Vue.use(Vuelidate);
 Vue.filter("date", dateFilter);
 
 new Vue({
